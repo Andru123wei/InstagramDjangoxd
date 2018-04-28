77b3e213356df1dd7f1ae9edc4c8ea4913d6bb71
Ático <atico@202CS1E17164405.local>
2018-04-28 13:07:53 -0500

    Creating footer

--- Instagram/settings.py
+++ Instagram/settings.py
@@ -120,8 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "services/static")
-MEDIA_ROOT = os.path.join(BASE_DIR, "services/static/media")
-
 STATIC_URL = '/static/'
-MEDIA_URL = "/media/"
+MEDIA_URL = "/images/"
+
+STATIC_ROOT = os.path.join(BASE_DIR, "services/static")
+MEDIA_ROOT = os.path.join(BASE_DIR, "services/static/images")
--- services/views.py
+++ services/views.py
@@ -1,10 +1,5 @@
 from django.shortcuts import render
-# Importando la funcion HttpResponse, sino no se puede usar
-from django.http import HttpResponse
 
-# Funcion index
+
 def index(request):
-    # Print sirve para imprimir
-    print("Soy un log de servidor.")
-    print(" -- Entrando al index --")
-    return HttpResponse("hola q hace.")
+    return render(request, "index.html")
